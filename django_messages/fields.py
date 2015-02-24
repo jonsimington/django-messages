e751b86a5af012f12e87b21defdab9e0ecadad44
jswv9@mst.edu <jswv9@mst.edu>
2015-02-23 20:32:00 -0600

    try stuffff

--- django_messages/fields.py
+++ django_messages/fields.py
@@ -40,9 +40,9 @@ class CommaSeparatedUserField(forms.Field):
             return value
         
         names = set(value.split(','))
-        names_set = set([name.strip() for name in names if name.strip()])
+        names_set = set([name.strip() for name.title() in names if name.strip()])
         users = list(User.objects.filter(**{'%s__in' % get_username_field(): names_set}))
-        unknown_names = names_set ^ set([getattr(user, get_username_field().title()) for user in users])
+        unknown_names = names_set ^ set([getattr(user, get_username_field()) for user in users])
         
         recipient_filter = self._recipient_filter
         invalid_users = []
